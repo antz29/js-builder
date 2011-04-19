5cbae831229afa4499bba5fce4fd145b554ed119
JohnLD <john.ledrew@thisisnumero.com>
2011-04-19 11:33:00 +0100

    Strips out jsbuilder comments on rendering files
    Built against Ant 1.8.2

--- src/org/antz29/jsbuilder/plugins/renderer/DefaultRenderer.java
+++ src/org/antz29/jsbuilder/plugins/renderer/DefaultRenderer.java
@@ -2,6 +2,8 @@ package org.antz29.jsbuilder.plugins.renderer;
 
 import java.io.IOException;
 import java.util.HashMap;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 import org.antz29.jsbuilder.Module;
 import org.antz29.jsbuilder.plugins.types.RendererPlugin;
@@ -20,6 +22,13 @@ public class DefaultRenderer implements RendererPlugin {
 			contents = "";
 			e.printStackTrace();
 		}
+		
+		// strip out JSBuilder comments
+		Pattern rx = Pattern.compile("^//\\s*#[A-Z]+:\\s*[a-z\\-\\,\\_]+$");
+		Matcher matcher = rx.matcher(contents);
+		contents = matcher.replaceAll("");
+		
+		contents = "\n// " + module.getFile().getName() + "\n" + contents;
 		
 		module.setRenderedContents(contents);
 		
